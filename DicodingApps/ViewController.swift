7e1c47f83b87132f07191d0f410ecd306610a090
abdhilabs <rizaabdhi@gmail.com>
2020-04-16 12:25:14 +0700

    Initial Commit

---
+++ DicodingApps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DicodingApps
+//
+//  Created by Abdhi on 16/04/20.
+//  Copyright © 2020 Abdhilabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
